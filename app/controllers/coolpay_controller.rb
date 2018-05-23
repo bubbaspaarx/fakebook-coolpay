c08321115c68a1f0d128dd9146d7d53dac9db283
Edward Sparks <edd_sparks@hotmail.com>
2018-05-23 12:25:50 +0100

    payment done

--- app/controllers/coolpay_controller.rb
+++ app/controllers/coolpay_controller.rb
@@ -1,9 +1,9 @@
 require 'rest-client'
 
 class CoolpayController < ApplicationController
+  before_action :authenticate, only: [:headers, :list, :add, :list_pay, :make_pay]
 
   def login
-
   end
 
   def list
@@ -11,7 +11,7 @@ class CoolpayController < ApplicationController
   end
 
   def add
-    @friend_name = params["coolpay"]["Friends Name"]
+    @friend_name = params[:coolpay][:name]
     values = {
       "recipient": {
         "name": "#{@friend_name}"
@@ -21,6 +21,27 @@ class CoolpayController < ApplicationController
     redirect_to list_path
   end
 
+  def list_pay
+    list_payments
+  end
+
+  def make_pay
+    get_rest
+    @amount = params[:coolpay][:amount]
+    @currency = params[:coolpay][:currency]
+    @recipient_id = @recipients.find {|x| x["name"] == params["coolpay"]["name"]}
+    values = {
+      "payment": {
+        "amount": "#{@amount}",
+        "currency": "#{@currency}",
+        "recipient_id": "#{@recipient_id["id"]}"
+      }
+    }.to_json
+    make_payment(values)
+    redirect_to list_pay_path
+  end
+
+
   private
 
   def base_url
@@ -28,42 +49,52 @@ class CoolpayController < ApplicationController
   end
 
   def authenticate
-    coolpay_user = ENV["coolpay_user"]
-    coolpay_key = ENV["coolpay_key"]
     values = {
-      "username": coolpay_user,
-      "apikey": coolpay_key
+      "username": ENV["coolpay_user"],
+      "apikey": ENV["coolpay_key"]
     }.to_json
 
-    headers = {
+    auth_headers = {
       :content_type => 'application/json'
     }
-    response = RestClient.post base_url + 'login', values, headers
+    response = RestClient.post base_url + 'login', values, auth_headers
 
     @token = JSON.parse(response)["token"]
-
-    return @token
   end
 
   def get_rest
-    authenticate
-
-    headers = {
-      :content_type => 'application/json',
-      :authorization => "Bearer #{@token}"
-    }
-    response = RestClient.get base_url + 'recipients', headers
-
+    if name_params = params[:coolpay]
+      @name = name_params[:name]
+      response = RestClient.get base_url + "recipients?name=#{@name.capitalize}", headers
+    else
+      response = RestClient.get base_url + 'recipients', headers
+    end
     @recipients = JSON[response]["recipients"]
   end
 
+
   def post_rest(values)
-    authenticate
+    response = RestClient.post base_url + 'recipients', values, headers
+  end
+
+  def list_payments
+    payments = RestClient.get base_url + 'payments', headers
+    friends = RestClient.get base_url + 'recipients', headers
+    @recipients = JSON[friends]["recipients"]
+    @payments = JSON[payments]["payments"]
+  end
+
+  def make_payment(values)
+    response = RestClient.post base_url + 'payments', values, headers
+  end
+
+
+  def headers
     headers = {
       :content_type => 'application/json',
       :authorization => "Bearer #{@token}"
     }
-    response = RestClient.post base_url + 'recipients', values, headers
   end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'coolpay/login', to: 'coolpay#login', as: 'login'
   get 'coolpay/list', to: 'coolpay#list', as: 'list'
+  post 'coolpay/list', to: 'coolpay#list', as: 'list_post'
   post 'coolpay/add', to: 'coolpay#add', as: 'add'
+  get 'coolpay/list_pay', to: 'coolpay#list_pay', as: 'list_pay'
+  post 'coolpay/make_pay', to: 'coolpay#make_pay', as: 'make_pay'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
